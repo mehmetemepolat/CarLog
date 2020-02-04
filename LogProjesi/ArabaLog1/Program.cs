fa9521c9ef505c1a1363ee11916ab81fa81563c9
Mehmet Emre Polat <56697232+mehmetemepolat@users.noreply.github.com>
2020-02-04 22:34:08 +0300

    Add files via upload

---
+++ LogProjesi/ArabaLog1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ArabaLog1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char yon;
+            int ivme = 0, i = 0;
+            do
+            {
+                yon = Console.ReadKey().KeyChar;
+                switch (yon)
+                {
+                    case 'w': Console.WriteLine(" " + DateTime.Now + " İleri"); break;
+                    case 'd': Console.WriteLine(" " + DateTime.Now + " Sağa Dönüş"); break;
+                    case 'a': Console.WriteLine(" " + DateTime.Now + " Sola Dönüş"); break;
+                    case 's': Console.WriteLine(" " + DateTime.Now + " Yavaşla"); ivme = ivme + 1; break;
+                    case 'o':
+                        {
+                            for (i = 0; i < 3; i++)
+                            {
+                                Console.Beep();
+                                Console.WriteLine(" " + DateTime.Now + " Sağa Sinyal");
+                                System.Threading.Thread.Sleep(850);
+                            }
+                        }
+                        break;
+                    case 'p':
+                        {
+                            for (i = 0; i < 3; i++)
+                            {
+                                Console.Beep();
+                                Console.WriteLine(" " + DateTime.Now + " Sola Sinyal");
+                                System.Threading.Thread.Sleep(850);
+                            }
+                        }
+                        break;
+
+
+
+                }
+            } while (ivme != 3);
+            if (ivme == 3)
+            {
+                Console.WriteLine(" Durdu");
+            }
+        }
+    }
+}
